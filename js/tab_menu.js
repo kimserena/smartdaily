b57067ef86501ba620e64e354fd18768b2f6a71b
serena <serena@delmetenz.com>
2020-01-16 18:27:09 +0900

    일보조회 업로드

---
+++ js/tab_menu.js
@@ -0,0 +1,31 @@
+//서브 탭 메뉴
+
+$(document).ready(function () {
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
+});
+
+function openInfo(evt, infoName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(infoName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+//탭_탭
+$(document).ready(function () {
+        $('ul.tabs li').click(function () {
+            var tab_id = $(this).attr('data-tab');
+            $('ul.tabs li').removeClass('current');
+            $('.tab-content').removeClass('current');
+            $(this).addClass('current');
+            $("#" + tab_id).addClass('current');
+        })
+    });
